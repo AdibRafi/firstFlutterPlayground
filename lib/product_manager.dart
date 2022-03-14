f952e3ba4d250e6d31b7c3fe550c3d1df0aed130
Abdjb <63862086+Abdjb@users.noreply.github.com>
2022-03-14 14:42:02 +0800

    first commit

--- lib/product_manager.dart
+++ lib/product_manager.dart
@@ -11,7 +11,7 @@ class ProductManager extends StatefulWidget {
 
   @override
   State<StatefulWidget> createState() {
-    print('[ProductManager Widget] createState()');
+    print('[ProductManager Widget] createState(sd)');
     return _ProductManagerState();
   }
 
